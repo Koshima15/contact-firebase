e844cf5958b79cbfaeae3b9a300ebe8de64f64fd
koshima <koshimabagga15@gmail.com>
2026-02-09 16:02:26 +0530

    Update files

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import { db } from './config/firebase';
 import ContactCard from './components/ContactCard';
 import AddAndUpdateContact from './components/AddAndUpdateContact';
 import useDisclose from './hooks/useDisclose';
-
-
 import { ToastContainer, toast } from 'react-toastify';
 import NotFound from './components/NotFound';
 const App = () => {
