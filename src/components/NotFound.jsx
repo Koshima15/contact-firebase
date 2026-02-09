656c109268389b38b3fcb29fa7770a430e0ab78b
koshima <koshimabagga15@gmail.com>
2026-02-09 15:40:03 +0530

    Update files

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import notFound from '../assets/Hands Contact.png'
 const NotFound = () => {
   return (
-    <div className='flex gap-3 items-center justify-center h-200'>
+    <div className='flex gap-3 items-center justify-center h-100'>
         <div>
 
         <img src={notFound} alt="" />
